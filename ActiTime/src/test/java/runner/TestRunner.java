6cec07a6a4ca7dba09331dcdd472401a04d6f36b
nikknikhil <62135476+nikknikhil@users.noreply.github.com>
2020-03-13 15:35:45 +0530

    Add files via upload

---
+++ ActiTime/src/test/java/runner/TestRunner.java
@@ -0,0 +1,14 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features = "src/test/java/features"
+,glue= {"LoginDefi"}
+)
+public class TestRunner {
+
+}
---
+++ ActiTime/src/test/java/stepDefi/LoginDefi.java
@@ -0,0 +1,29 @@
+package stepDefi;
+
+import cucumber.api.java.en.*;
+
+public class LoginDefi {
+	
+	@Given("^user is  on homepage$")
+	public void user_is_on_homepage()  {
+	    
+	}
+
+	@When("^user navigates to Login Page$")
+	public void user_navigates_to_Login_Page()  {
+	  
+	}
+
+	@When("^user enters username and Password$")
+	public void user_enters_username_and_Password()  {
+	  
+	}
+
+	@Then("^success message is displayed$")
+	public void success_message_is_displayed() {
+	  
+	}
+
+
+	
+}
